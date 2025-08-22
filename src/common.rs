0c052026747a12f0371bbc80bd9c0d4e5eafe040
baggiodick <baggio.dick@gmail.com>
2025-08-23 03:25:37 +0800

    更新組態檔案

--- src/common.rs
+++ src/common.rs
@@ -1014,7 +1014,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "https://dic.cloudns.org".to_owned()
 }
 
 #[inline]
